196ef6e389590904095ba60a484c2d5ddb81833e
Eissa Saber <eissapk44@gmail.com>
2021-10-30 22:07:29 +0200

    fixed nested node modules path

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@
 
 const process = require("process");
 process.on("unhandledRejection", err => {
-  throw new Error(err)
+  throw new Error(err);
 });
 
 // check node version
@@ -20,11 +20,7 @@ const spawn = require("cross-spawn");
 const script = process.argv[2];
 
 if (typeof script !== undefined && ["dev", "build"].includes(script)) {
-  const result = spawn.sync(path.resolve(__dirname, "../node_modules/gulp/bin/gulp.js"),
-    [script, "--gulpfile", path.resolve(__dirname, "../tasks")], {
-      stdio: "inherit"
-    }
-  );
+  const result = spawn.sync("gulp", [script, "--gulpfile", path.resolve(__dirname, "../tasks")], { stdio: "inherit" });
 
   if (result.signal) {
     if (result.signal === "SIGKILL" || result.signal === "SIGTERM") console.log("Failed to run " + script + " script!");
--- tasks/bundle.js
+++ tasks/bundle.js
@@ -1,21 +1,18 @@
-const { exec } = require("child_process");
 const logger = require("./logger");
 const reload = require("./reload");
-const path = require("path");
 const spawn = require("cross-spawn");
 
 const bundle = mode => {
   const taskName = "bundle";
   return new Promise((resolve, reject) => {
-    const file = path.resolve(__dirname, "../node_modules/webpack/bin/webpack.js");
     logger(taskName, "start");
     if (mode === "development") {
-      spawn.sync(file, ["--env", "dev"], { stdio: "inherit" } );
+      spawn.sync("webpack", ["--env", "dev"], { stdio: "inherit" } );
       reload();
       resolve();
       logger(taskName, "end");
     } else if (mode === "production") {
-      spawn.sync(file, { stdio: "inherit" } );
+      spawn.sync("webpack", { stdio: "inherit" } );
       resolve();
       logger(taskName, "end");
     }
