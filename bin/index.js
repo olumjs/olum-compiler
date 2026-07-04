956b2b93e5f6e41522ab122386d1bc5f421c08b6
Eissa Saber <eissapk@zohomail.com>
2026-07-04 13:12:18 +0300

    handle onMount hook and its imports

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,9 @@ const path = require("path");
 const spawn = require("../lib/cross-spawn");
 const script = process.argv[2];
 
+if (script == "dev") process.env.NODE_ENV = "development";
+else if (script == "build") process.env.NODE_ENV = "production";
+
 if (typeof script !== undefined && ["dev", "build"].includes(script)) {
   const result = spawn.sync("node", [path.resolve(__dirname, script + ".js")], { stdio: "inherit" });
 
